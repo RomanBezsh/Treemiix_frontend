c9b0a0b0900d7d43cf118204089d57d34b491b07
Roman Bezshyiko <antikys189@gmail.com>
2026-08-24 16:56:09 +0300

    dima

---
+++ app/dima/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+
+
+const Dima = () => {
+    return(
+        <img className="flex-1" src="https://i.pinimg.com/736x/3c/a8/39/3ca839d2e2b61d5a8ebb10cadd887a8f.jpg" alt="Діма"/>
+    )
+}
+
+
+export default Dima
