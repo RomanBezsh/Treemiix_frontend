74a695dc137103a2d964aaa52cfb4dac74734901
Roman Bezshyiko <antikys189@gmail.com>
2026-09-02 18:42:20 +0300

    fix: allow any hostname

--- next.config.ts
+++ next.config.ts
@@ -5,31 +5,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cdn.new-brz.net',
+        hostname: '**',
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com', // <-- Добавь эту строчку
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.hp.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'br-media.hptiendaenlinea.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pisces.bbystatic.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.gamescom.gr',
+        protocol: 'http',
+        hostname: '**',
       },
     ],
   },
